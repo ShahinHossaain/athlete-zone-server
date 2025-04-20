6bb2247a9c35030cd11e2283959518026e13f142
ShahinHossaain <mdshahinhossaain@gmail.com>
2025-04-20 23:12:21 +0600

    code updated

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
+const stripe = require('stripe')(process.env.PAYMENT_key);
+
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require("jsonwebtoken");
 const morgan = require('morgan');
 
-require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -13,8 +17,9 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-const uri = `mongodb+srv://${process.env.DB_name}:${process.env.DB_pass}@cluster0.98whfas.mongodb.net/?retryWrites=true&w=majority`;
 
+
+const uri = `mongodb+srv://mdshahinhossaain:${process.env.DB_pass}@shahincluster.966gsck.mongodb.net/?retryWrites=true&w=majority&appName=shahinCluster`;
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -27,13 +32,11 @@ const client = new MongoClient(uri, {
 });
 
 const userVerify = (req, res, next) => {
-    // console.log('called');
-    // console.log('headers', req.headers.authorization);
+    console.log('headers', req.headers.authorization);
 
     const auth = req.headers.authorization;
     if (!auth) return res.status(401).send('bolod naki, token koi tor')
     const token = auth.split(' ')[1];
-    // console.log('tata', token);
 
     jwt.verify(token, process.env.SECRET_key, (err, decoded) => {
         if (err) {
@@ -50,23 +53,35 @@ async function run() {
         const database = client.db('sportsDB');
         const usersCollection = database.collection('users');
         const classCollection = database.collection('classes');
+        const enrolledCollection = database.collection('enrolled');
+
+        // Generate Client secret 
+        app.post('/create-payment-intent', userVerify, async (req, res) => {
+            const price = req.body;
+            if (price) {
+                const amount = parseFloat(price.price) * 100;
+                const paymentIntent = await stripe.paymentIntents.create({
+                    amount: amount,
+                    currency: 'USD',
+                    payment_method_types: ['card']
+                });
+                res.send({ clientSecret: paymentIntent.client_secret });
+            }
+        });
+
 
         // TODO: insert token to env variable  
         app.post('/jwt', async (req, res) => {
             const email = req.body;
-            // console.log(email, process.env.SECRET_key);
             const token = jwt.sign(email, process.env.SECRET_key, { expiresIn: '7d' })
-            // console.log('tkn', token);
             res.send(token);
         })
 
         // user start 
         app.get('/users/:email', async (req, res) => {
             const email = req.params.email;
-            // console.log(email);
             const result = await usersCollection.findOne({ email: email });
             // const result = await cursor.toArray();
-            // console.log(result);
             res.send(result);
         });
 
@@ -74,7 +89,6 @@ async function run() {
         // app.patch('/users/selectedClasses/:email', async (req, res) => {
         //     const email = req.params.email;
         //     const id = req.body.selectedClass;
-        //     console.log('id', id);
         //     try {
         //         const result = await usersCollection.findOneAndUpdate(
         //             { email: email },
@@ -94,9 +108,9 @@ async function run() {
         // });
 
         app.patch('/users/selectedClasses/:email', async (req, res) => {
+
             const email = req.params.email;
             const id = req.body.selectedClass;
-            console.log('id', id);
             try {
                 const result = await usersCollection.findOneAndUpdate(
                     { email: email },
@@ -110,7 +124,6 @@ async function run() {
                     res.status(404).send({ error: 'User not found' });
                 }
             } catch (error) {
-                console.error('Error updating selected classes:', error);
                 res.status(500).send({ error: 'Internal server error' });
             }
         });
@@ -121,9 +134,12 @@ async function run() {
 
 
         app.get('/users', async (req, res) => {
-            const cursor = usersCollection.find();
+            let query = {};
+            if (req?.query?.role) {
+                query = { role: req.query.role };
+            }
+            const cursor = usersCollection.find(query);
             const result = await cursor.toArray();
-            // console.log(result);
             res.send(result);
         });
 
@@ -132,7 +148,6 @@ async function run() {
         //     const filter = { _id: new ObjectId(id) };
         //     const options = { upsert: true };
         //     const updatedUser = req.body;
-        //     console.log(req.body);
         //     const updateDoc = {
         //         $set: updatedUser,
         //     };
@@ -145,7 +160,6 @@ async function run() {
         //     const filter = { _id: new ObjectId(id) };
         //     const options = { upsert: true };
         //     const updatedUser = req.body;
-        //     console.log(req.body);
         //     const updateDoc = {
         //         $set: updatedUser,
         //     };
@@ -157,7 +171,6 @@ async function run() {
             const filter = { email: email };
             const options = { upsert: true };
             const updatedUser = req.body;
-            // console.log(req.body);
             const updateDoc = {
                 $set: updatedUser,
             };
@@ -169,7 +182,6 @@ async function run() {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const role = req.body;
-            // console.log(role);
             const options = { upsert: true };
             const updateDoc = {
                 $set: {
@@ -177,7 +189,6 @@ async function run() {
                 }
             };
             const result = await usersCollection.updateOne(filter, updateDoc, options);
-            // console.log(result);
             res.send(result);
         });
 
@@ -187,9 +198,7 @@ async function run() {
         //     const id = req.params.id;
         //     const email = req.query.email;
 
-        //     console.log(id, email);
         //     const user = await usersCollection.findOne({ email: email });
-        //     console.log(user);
         //     // Your code to handle the enrollment logic goes here
 
         //     // Send a response indicating the enrollment is successful
@@ -216,17 +225,48 @@ async function run() {
                 await usersCollection.updateOne({ email: email }, { $set: { selectedClasses: user.selectedClasses } });
 
                 // const classItem = await classCollection.findOne({ id: new ObjectId(id) });
-                // console.log(cla);
 
-                const result = await classCollection.findOne({ _id: new ObjectId(id) }, { projection: { enrolled: 1, _id: 0 } });
-                console.log('NNNNNNNNNNNNNNNNNN', result.enrolled);
+                // old code start 
+
+                // const result = await classCollection.findOne({ _id: new ObjectId(id) }, { projection: { enrolled: 1, _id: 0 } });
+
+                // const updateDoc = {
+                //     $set: {
+                //         enrolled: ++result.enrolled
+                //     }
+                // }
+                // await classCollection.updateOne({ _id: new ObjectId(id) }, updateDoc);
+
+                // old code finish 
+
+                // new code start 
+                const classItem = await classCollection.findOne({ _id: new ObjectId(id) });
+
+                if (!classItem) {
+                    return res.status(404).send({ success: false, message: 'Class not found' });
+                }
+
+                const availableSeats = parseInt(classItem.availableSeats);
+                const enrolled = parseInt(classItem.enrolled || 0);
+
+                if (availableSeats <= 0) {
+                    return res.status(400).send({ success: false, message: 'No seats available' });
+                }
 
                 const updateDoc = {
                     $set: {
-                        enrolled: ++result.enrolled
+                        availableSeats: availableSeats - 1,
+                        enrolled: enrolled + 1
                     }
-                }
-                const updateEnroll = await classCollection.updateOne({ _id: new ObjectId(id) }, updateDoc);
+                };
+
+                const result = await classCollection.updateOne(
+                    { _id: new ObjectId(id) },
+                    updateDoc
+                );
+                // new code finish 
+
+
                 // Send a response indicating the enrollment is successful
                 res.status(200).json({ message: 'Enrollment successful' });
             } catch (error) {
@@ -246,10 +286,16 @@ async function run() {
 
         // class start 
 
+        app.get("/class/:id", async (req, res) => {
+            console.log("req send")
+            const result = await classCollection.findOne({ _id: new ObjectId(req.params.id) });
+            console.log(result, "result");
+            res.send(result);
+        })
+
 
         app.post('/classes', async (req, res) => {
             const newClass = req.body;
-            // console.log(newClass);
             const result = await classCollection.insertOne(newClass);
             res.send(result);
         })
@@ -259,7 +305,6 @@ async function run() {
             const isEnabled = req.query.isEnabled;
             const email = req.query.email;
 
-            console.log('vvvvvvvv', isEnabled, email);
             const id = req.params.id;
 
 
@@ -269,14 +314,11 @@ async function run() {
 
                 const classId = Object.keys(selectedClass)[0];
                 const value = Object.values(selectedClass)[0];
-                console.log(classId, value);
-                console.log(typeof value, typeof isEnabled);
                 if (classId === id && value === parseInt(isEnabled)) {
 
                     return classId
                 }
                 return
-                // console.log('selectedClass', selectedClass);
             });
             if (!matchingClass) {
                 // Send empty response if no match is found
@@ -285,7 +327,6 @@ async function run() {
             else {
 
                 const id = Object.keys(matchingClass);
-                console.log("idddd", id[0]);
                 const filter = { _id: new ObjectId(id[0]) };
                 const result = await classCollection.findOne(filter);
                 res.send(result);
@@ -295,17 +336,13 @@ async function run() {
         //     const isEnabled = req.query.isEnabled;
         //     const email = req.query.email;
 
-        //     console.log('vvvvvvvv', isEnabled, email);
         //     const id = req.params.id;
 
         //     const user = await usersCollection.findOne({ email: email });
-        //     console.log(user.selectedClasses, 'id', id, 'c', ++c);
 
         //     const matchingClass = user.selectedClasses.find((selectedClass) => {
         //         const [classId, value] = Object.entries(selectedClass)[0];
-        //         console.log(classId, value);
         //         if (classId === id && value === 0) {
-        //             console.log('kattu', classId, 'id', id, value);
         //             return selectedClass; // Return the entire selectedClass object
         //         }
         //     });
@@ -313,7 +350,6 @@ async function run() {
 
         //     if (matchingClass) {
         //         const id = Object.keys(matchingClass);
-        //         console.log("idddd", id[0]);
         //         const filter = { _id: new ObjectId(id[0]) };
         //         const result = await classCollection.findOne(filter);
         //         res.send(result);
@@ -326,37 +362,82 @@ async function run() {
 
         app.get('/classes', async (req, res) => {
 
+
             if (req.query?.fromManageClasses) {
                 const cursor = classCollection.find();
                 const result = await cursor.toArray();
                 res.send(result);
             }
-            else {
-                let query = { status: 'approve' };
-                if (req.query?.email) {
-                    query = { instructorEmail: req.query.email, status: 'approve' }
-                }
-
-                const count = await classCollection.countDocuments();
-
-                let options = { limit: count }
-                if (req.query?.limit) {
-                    options = { limit: parseInt(req.query.limit) }
-                }
-
-
-                const cursor = classCollection.find(query, options);
+            else if (req.query?.id) {
+                const id = req.query.id;
+                const filter = { _id: new ObjectId(id) };
+                const result = await classCollection.findOne(filter);
+                res.send(result);
+            }
+            else if (req.query?.all) {
+                const email = req.query.email;
+                all = req.query.all;
+                // const status = req.query.status;
+                const filter = { instructorEmail: email }
+                const cursor = classCollection.find(filter);
                 const result = await cursor.toArray();
                 res.send(result);
+            }
+            else {
+                const email = req.query.email;
+                let query = { status: 'approve' };
+                if (req.query?.email) {
+                    query = { status: 'approve' }
+                }
+
+                const user = await usersCollection.findOne({ email: email });
+
+                if (!user) {
+                    return res.status(404).json({ message: "User not foundd" });
+                }
+
+                let selectedClassIds = [];
+
+                if (user.selectedClasses && Array.isArray(user.selectedClasses)) {
+                    selectedClassIds = user.selectedClasses.map(item => {
+                        return Object.keys(item)[0];
+                    });
+                }
+
+                let options = {};
+                if (req.query?.limit) {
+                    options.limit = parseInt(req.query.limit);
+                }
+
+                // Step 3: Find classes excluding those IDs
+                const remainingClasses = await classCollection
+                    .find({ _id: { $nin: selectedClassIds.map(id => new ObjectId(id)) } });
+
+                if (options.limit) {
+                    remainingClasses.limit(options.limit);
+                }
+
+                const classes = await remainingClasses.toArray();
+                res.json(classes);
             }
 
         })
 
+        app.get('/class', async (req, res) => {
+            try {
+                const email = req.query.email;
+                const classes = await classCollection.find({ instructorEmail: email }).toArray();
+
+                res.send(classes);
+            } catch (error) {
+                console.error("Error fetching classes:", error);
+                res.status(500).send("Internal Server Error");
+            }
+        });
 
 
         app.get('/feedback/:id', userVerify, async (req, res) => {
             const decodedEmail = req.decoded?.email;
-            console.log('decodedEmail', decodedEmail, req.query.email);
             if (req?.query?.email) {
                 if (decodedEmail !== req.query?.email) return res.status(403).send({ error: true, message: "Forbidden access" });
             }
@@ -370,7 +451,6 @@ async function run() {
         //     const id = req.params.id;
         //     const filter = { _id: new ObjectId(id) };
         //     const data = req.body;
-        //     console.log(data);
 
         //     const options = { upsert: true };
         //     const updateDoc = {
@@ -382,7 +462,6 @@ async function run() {
         //         }
         //     };
         //     const result = await classCollection.updateOne(filter, updateDoc, options);
-        //     console.log(result);
         //     res.send(result);
         // });
 
@@ -392,7 +471,6 @@ async function run() {
                 const id = req.params.id;
                 const filter = { _id: new ObjectId(id) };
                 const data = req.body;
-                console.log('dataaa', data);
                 let updateDoc;
                 if (data.status === 'approve') {
                     updateDoc = {
@@ -421,23 +499,19 @@ async function run() {
                 // }
 
                 const result = await classCollection.updateOne(filter, updateDoc, options);
-                console.log(result);
                 res.send(result);
             } catch (error) {
-                console.error(error);
+                // console.error(error);
                 res.status(500).send('An error occurred');
             }
         });
 
         // app.delete("/classDelete/:id", async (req, res) => {
         //     const { id } = req.params;
-        //     console.log('iddd', id);
         //     const email = req?.query?.email; // Assuming you have implemented user authentication
-        //     console.log(req.query);
         //     try {
         //         // Find the user by email
         //         const user = await usersCollection.findOne({ email });
-        //         console.log(user);
 
         //         if (!user) {
         //             return res.status(404).json({ message: "User not found" });
@@ -453,7 +527,6 @@ async function run() {
 
         //         res.status(200).json({ message: "Class removed successfully" });
         //     } catch (error) {
-        //         console.log("Error removing class:", error);
         //         res.status(500).json({ message: "Server error" });
         //     }
         // });
@@ -480,13 +553,18 @@ async function run() {
 
                 res.status(200).json({ message: "Class removed successfully" });
             } catch (error) {
-                console.log("Error removing class:", error);
                 res.status(500).json({ message: "Server error" });
             }
         });
 
 
+        // enrolled 
 
+        app.post('/enrolled', async (req, res) => {
+            const data = req.body;
+            const result = await enrolledCollection.insertOne(data);
+            res.send(result);
+        })
 
 
 
@@ -503,5 +581,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-
+    console.log(`Example app listening on port ${port}`);
 });
